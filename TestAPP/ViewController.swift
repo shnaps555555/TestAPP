9796979fbe23d0f93e8b2f0991b0e2135abeb028
shnaps <gusein.mss@gmail.com>
2018-09-20 16:53:37 +0300

    Initial Commit

---
+++ TestAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAPP
+//
+//  Created by Aliaksei on 9/20/18.
+//  Copyright © 2018 Medibook Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
